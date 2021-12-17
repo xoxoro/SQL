bfdc1262e06e61398c60a4200d57b78dca07ba59
심유정 <shimuniv@gmail.com>
2021-12-17 10:11:43 +0900

    과제

--- Practice/Practice01.sql
+++ Practice/Practice01.sql
@@ -42,6 +42,45 @@ select  first_name,
         NVL(commission_pct,0)
 from employees
 order by salary desc;
-        
+  
+--문제6        
+--부서번호가 10, 90, 100 인 직원의 이름, 월급, 입사일, 부서번호를 나타내시오
+--입사일은 1977-12 와 같이 표시하시오
+select  first_name,
+        salary,
+        to_char(hire_date,'YYYY-MM'),
+        department_id
+from employees
+where department_id in (10,90,100);
+
+--문제7.
+--이름(first_name)에 S 또는 s 가 들어가는 직원의 이름, 월급을 나타내시오
+select  first_name,
+        salary
+from employees
+where first_name like lower('%S%');
+--initcap은 왜 안돼
+
+--문제8.
+--전체 부서를 출력하려고 합니다. 순서는 부서이름이 긴 순서대로 출력해 보세오
+select *
+from departments
+order by length(department_name) asc;
 
 
+--문제9.
+--(정확하지 않지만, 지사가 있을 것으로 예상되는 나라들을) 나라이름을 대문자로 출력하고
+--올림차순(ASC)으로 정렬해 보세오.
+select upper(country_name)
+from countries
+order by 'country_name' asc;
+
+--문제10.
+--입사일이 03/12/31 일 이전 입사한 직원의 이름, 월급, 전화 번호, 입사일을 출력하세요
+--전화번호는 545-343-3433 과 같은 형태로 출력하시오
+select  first_name 이름,
+        salary 월급,
+        replace(phone_number,'.','-') 전화번호,
+        hire_date 입사일
+from employees
+where hire_date > '03/12/31';
