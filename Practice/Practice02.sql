2a042dc8aa55cfcdbf300b0ef36364a9c1648b85
심유정 <shimuniv@gmail.com>
2021-12-21 14:06:55 +0900

    과제

--- Practice/Practice02.sql
+++ Practice/Practice02.sql
@@ -28,3 +28,16 @@ order by hire_date desc;
 
 select to_char(max(hire_date),'YYYY"년"MM"월"DD"일"') 마지막입사일
 from employees;
+
+--문4)부서별로 평균임금, 최고임금, 최저임금을 부서아이디(department_id)와 함께 출력합니다.
+--정렬순서는 부서번호(department_id) 내림차순입니다
+select  round(avg(salary),0),
+        max(salary),min(salary),
+        department_id
+from employees
+group by department_id
+order by department_id desc;
+
+--문5)업무(job_id)별로 평균임금, 최고임금, 최저임금을 업무아이디(job_id)와 함께 출력하고 정렬
+--순서는 최저임금 내림차순, 평균임금(소수점 반올림), 오름차순 순입니다.
+--(정렬순서는 최소임금 2500 구간일때 확인해볼 것)
