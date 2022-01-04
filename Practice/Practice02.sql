7e8fab7db18e7f341150bc912c56b9b98c5dc92c
심유정 <shimuniv@gmail.com>
2022-01-04 22:26:17 +0900

    과제

--- Practice/Practice02.sql
+++ Practice/Practice02.sql
@@ -51,3 +51,74 @@ group by job_id
 having min(salary) >= 2500
 order by min(salary) desc, max(salary) asc;
 
+--문제6.
+--가장 오래 근속한 직원의 입사일은 언제인가요? 다음 형식으로 출력해주세요.
+--예) 2001-01-13 토요일 
+select  first_name,
+        to_char(min(hire_date), 'yyyy-mm-dd day')
+from employees;
+
+select  first_name, 
+        hire_date
+from employees
+where hire_date = ( select min(hire_date)
+                    from employees);
+                    
+--문제7.
+--평균임금과 최저임금의 차이가 2000 미만인 부서(department_id), 
+--평균임금, 최저임금 
+--그리고 (평균임금 – 최저임금)를 (평균임금 – 최저임금)의 내림차순으로 정렬해서 출력하세요.
+select  department_id,
+        avg(salary),
+        min(salary),
+        avg(salary) - min(salary)
+from employees
+group by department_id
+having avg(salary) - min(salary) < 2000
+order by avg(salary) - min(salary) desc;
+
+--문제8.
+--업무(JOBS)별로 최고임금과 최저임금의 차이를 출력해보세요.
+--차이를 확인할 수 있도록 내림차순으로 정렬하세요
+select  job_id,
+        max(salary) - min(salary)
+from employees
+group by job_id
+order by max(salary)-min(salary) desc;
+
+
+
+--문제9
+--2005년 이후 입사자중 관리자별로 평균급여 최소급여 최대급여를 알아보려고 한다.
+--출력은 관리자별로 평균급여가 5000이상 중에 평균급여 최소급여 최대급여를 출력합니다.
+--평균급여의 내림차순으로 정렬하고 평균급여는 소수점 첫째짜리에서 반올림 하여 출력합니다.
+select  manager_id,
+        round(avg(salary),1),
+        min(salary),
+        max(salary)
+from employees
+where hire_date > '05/01/01'
+group by manager_id
+having avg(salary) >= 5000
+order by avg(salary) desc;
+
+
+
+
+
+문제10
+아래회사는 보너스 지급을 위해 직원을 입사일 기준으로 나눌려고 합니다. 
+입사일이 02/12/31일 이전이면 '창립맴버, 03년은 '03년입사’, 04년은 ‘04년입사’ 
+이후입사자는 ‘상장이후입사’ optDate 컬럼의 데이터로 출력하세요.
+정렬은 입사일로 오름차순으로 정렬합니다.
+select  first_name 이름,
+        salary 급여,
+        case when hire_date < '03/01/01' then '창립멤버'
+             when hire_date < '04/01/01' then '03년입사'
+             else '상장이후'
+        end optDate,
+        hire_date
+from employees
+order by hire_date asc;
+
+
